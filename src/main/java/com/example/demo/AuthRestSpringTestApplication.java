f3b3ef93682f535e23ac9fc0606a5530ad4ca59e
Piero Aldinucci <piero.aldinucci@gmail.com>
2023-09-19 11:12:02 +0200

    First commit

---
+++ src/main/java/com/example/demo/AuthRestSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRestSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthRestSpringTestApplication.class, args);
+	}
+
+}
